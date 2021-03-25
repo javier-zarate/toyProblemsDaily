58e312520950ec0209326c284bed838a78cc5c69
Javier Zarate <javalzar17@gmail.com>
2021-03-25 12:22:46 -0700

    Time: 196 ms (97.12%  Space: 49.4 MB (21.47%) - LeetHub

--- palindrome-number/palindrome-number.js
+++ palindrome-number/palindrome-number.js
@@ -3,5 +3,16 @@
  * @return {boolean}
  */
 let isPalindrome = (x) => {
-	return x.toString() === x.toString().split('').reverse().join('');
+	// any negative number will never be a palindrome
+	if (x < 0) return false;
+
+	// convert numbet to array
+	const numArr = String(x).split('');
+
+	// check matches fromt the outside in
+	for (let i = 0; i < numArr.length; i++) {
+		if (numArr[i] !== numArr[numArr.length - (1 + i)]) return false;
+	}
+
+	return true;
 };
