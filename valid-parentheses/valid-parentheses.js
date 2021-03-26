05c63191f07c6643c4fc1f30ed5090d765b90e17
Javier Zarate <javalzar17@gmail.com>
2021-03-26 12:21:34 -0700

    Time: 72 ms (95.76%), Space: 38.8 MB (73.25%) - LeetHub

--- valid-parentheses/valid-parentheses.js
+++ valid-parentheses/valid-parentheses.js
@@ -2,27 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
-	let tracker = [];
+let map = {
+	'(': ')',
+	'[': ']',
+	'{': '}',
+};
+
+let isValid = function (s) {
+	let stack = [];
 
 	for (let i = 0; i < s.length; i++) {
-		let curr = s[i];
+		let el = s[i];
 
-		if (curr === '(' || curr === '{' || curr === '[') {
-			tracker.push(curr);
-		}
-
-		if (curr === ')' || curr === '}' || curr === ']') {
-			let last = tracker.pop(curr);
-
-			if (curr === ')' && last === '(') continue;
-			else if (curr === '}' && last === '{') continue;
-			else if (curr === ']' && last === '[') continue;
-			else return false;
+		// if element is in map it is added to the stack
+		if (map[el]) {
+			stack.push(map[el]);
+		} else {
+			// if element is not is map it should me match with the top of the stack
+			if (el !== stack.pop()) {
+				return false;
+			}
 		}
 	}
-	// case were all elements in string were opening brackets
-	if (tracker.length !== 0) return false;
-
-	return true;
+  //if that stack is not empty there was an un closed bracket at some point
+	return stack.length === 0;
 };
