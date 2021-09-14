0b2caf4bbc8ba33f197145c8dc8efb7e973977d5
Javier Zarate <javalzar17@gmail.com>
2021-09-13 19:24:33 -0700

    optimized solution

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -3,20 +3,35 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {    
-  // iterate over the numbers
-  for (let i = 0; i < nums.length; i++) {
-    // nested array checking the rest of array for sum
-    for (let j = i + 1; j < nums.length; j++) {
-      // check if i and j add up to sum 
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-  }
-};
+/* Brute for solution */
+// var twoSum = function(nums, target) {
+//   // iterate over the numbers
+//   for (let i = 0; i < nums.length; i++) {
+//     // nested array checking the rest of array for sum
+//     for (let j = i + 1; j < nums.length; j++) {
+//       // check if i and j add up to sum
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+// Time  complexity: O(n^2)
+// Space Complexity: O(1)
 
-// Input: array, tartget 
-// output: indices or sum to target [i, k]
-// contraints: listed, only one solution first slution you return 
-// edge cases: 
+/* Optimized solution */
+var twoSum = function (nums, target) {
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (hash[target - num] !== undefined) {
+      return [i, hash[target - num]];
+    }
+    hash[num] = i;
+  }
+  return [];
+}
+
+// this solution is faster
+// Time Complexity: O(n) -> traverse arr once
+// Space Complexity: O(n) -> worst case store all numbers in hashmap
