e6890d42d5d3736f84b58498f919f3ea416ef60c
Javier Zarate <javalzar17@gmail.com>
2021-09-13 19:19:07 -0700

    Time: 76 ms (86.26%), Space: 40.3 MB (47.31%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -3,20 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {    
-  // iterate over the numbers
+var twoSum = function (nums, target) {
+  const hash = {};
   for (let i = 0; i < nums.length; i++) {
-    // nested array checking the rest of array for sum
-    for (let j = i + 1; j < nums.length; j++) {
-      // check if i and j add up to sum 
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+    const num = nums[i];
+    if (hash[target - num] !== undefined) {
+      return [i, hash[target - num]];
     }
+    hash[num] = i;
   }
-};
-
-// Input: array, tartget 
-// output: indices or sum to target [i, k]
-// contraints: listed, only one solution first slution you return 
-// edge cases: 
+  return [];
+}
