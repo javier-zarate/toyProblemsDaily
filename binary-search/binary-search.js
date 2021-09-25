a275b3694305405837b3b609c2d9ed97ead247af
Javier Zarate <javalzar17@gmail.com>
2021-09-24 19:35:01 -0700

    Time: 72 ms (92.04%), Space: 42.5 MB (15.12%) - LeetHub

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -3,23 +3,22 @@
  * @param {number} target
  * @return {number}
  */
+
+// search for target in SORTED array.
+// time complexity must be O(log n)
 var search = function(nums, target) {
-    
-    const binarySearch = (start, end) =>  {
-        // base case for target not in arr
+
+    const binarySearch = (start, end) => {
         if (start > end) return -1;
-        
-        //determine mid 
-        let mid = Math.floor((start + end)/ 2);
-        
-        if (nums[mid] === target) return mid; 
-        
+        let mid = Math.floor((start + end) / 2);
+
+        if (nums[mid] === target) return mid;
+
         if (nums[mid] > target) {
-            return binarySearch(start, mid - 1); 
-        } else {
-            return binarySearch(mid + 1, end); 
+            return binarySearch(start, mid - 1);
         }
-    }
-    
-    return binarySearch(0, nums.length - 1);
+        return binarySearch(mid + 1, end);
+    };
+
+    return binarySearch(0, nums.length - 1) ;
 };
