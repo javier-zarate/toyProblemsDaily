8488a1e994d07a2277e7c4da9d44b768c913f6cb
Javier Zarate <javalzar17@gmail.com>
2021-09-27 17:05:18 -0700

    bfs and dfs traversal on graphs

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -39,6 +39,59 @@ class Graph {
   }
 }
 
+// breadth first  search traversal
+// search "level by level"
+// result shows the nodes traversed
+Graph.prototype.bfs = function(start) {
+  const queue = [start];
+  const result = [];
+  const visited = {};
+  visited[start] = true;
+
+  while (queue.length) {
+    currentVertex = queue.shift();
+    result.push(currentVertex);
+
+    this.adjacencyList.get(currentVertex).forEach(neighbor => {
+      console.log(neighbor)
+      if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        queue.push(neighbor);
+      }
+    });
+  }
+
+  return result;
+}
+
+
+// Depth First Search
+// going "DOWN" the graph then back tracking and repeating
+Graph.prototype.dfsRecursive = function(start) {
+  let result = [];
+  let visited = {};
+  let stack = [];
+
+  const adjacencyList = this.adjacencyList;
+
+  (function dfs(vertex) {
+    if (!vertex) return null;
+
+    visited[vertex] = true;
+    result.push(vertex);
+
+    adjacencyList.get(vertex).forEach(neighbor => {
+      if (!visited[neighbor]) {
+        return dfs(neighbor);
+      }
+    });
+  })(start);
+  return result;
+
+  console.log(this.adjacencyList.get(start));
+  return 'heeey';
+}
+
 let graph = new Graph();
 
 graph.addVertex(1)
@@ -51,10 +104,11 @@ graph.addUndirectedEdge(2,3)
 graph.addUndirectedEdge(3,4)
 graph.addUndirectedEdge(1,4)
 
-console.log(graph)
+// console.log(graph)
 
-graph.removeEdge(1,4);
-console.log(graph)
+// graph.removeEdge(1,4);
+// console.log(graph)
 
-graph.removeVertex(1);
-console.log(graph);
+// console.log(graph.bfs(1))
+// console.log(graph)
+console.log(graph.dfsRecursive(1))
