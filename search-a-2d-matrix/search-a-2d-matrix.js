2d7cceefc99cd36b7499cbaba5884c3460937d95
Javier Zarate <javalzar17@gmail.com>
2021-10-02 11:50:03 -0700

    more effiecient implementation

--- search-a-2d-matrix/search-a-2d-matrix.js
+++ search-a-2d-matrix/search-a-2d-matrix.js
@@ -3,29 +3,36 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
-    let isFound = false;
-    
-    for (const row of matrix) {
-        const first = row[0], last = row[row.length - 1];
-        if (first <= target && last >= target) {
-            isFound = binarySearch(row, target, 0, row.length - 1);
-        }
-    }
-    
-    return isFound;
+ var searchMatrix = function(matrix, target) {
+    return binarySearchRows(matrix, target, 0, matrix.length - 1);
 };
 
-const binarySearch = (arr, target, start, end) => {
-    // base case 
+const binarySearchRowWithinRange = (arr, target, start, end) => {
+    // base case
     if (start > end) return false;
-    
-    const mid = Math.floor((end + start) / 2); 
-    
+
+    const mid = Math.floor((end + start) / 2);
+
     if (arr[mid] === target) return true;
-    
+
     if (arr[mid] > target) {
-        return binarySearch(arr, target, start, mid - 1);
+        return binarySearchRowWithinRange(arr, target, start, mid - 1);
     }
-    return binarySearch(arr, target, mid + 1, end);
+    return binarySearchRowWithinRange(arr, target, mid + 1, end);
+};
+
+const binarySearchRows = (matrix, target, start, end) => {
+    if (start > end) return false;
+
+    const mid = Math.floor((end + start) / 2);
+
+    if (matrix[mid][0] === target) return true;
+
+    if (matrix[mid][0] < target && matrix[mid][matrix[mid].length - 1] >= target) {
+        return binarySearchRowWithinRange(matrix[mid], target, 0, matrix[mid].length -1);
+    }
+
+    if (matrix[mid][0] > target) return binarySearchRows(matrix, target, start, mid -1);
+
+    return binarySearchRows(matrix, target, mid + 1, end);
 }
