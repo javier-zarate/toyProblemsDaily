3519b8e489fffec0234fb50ebca066bc2d0f5e83
Javier Zarate <javalzar17@gmail.com>
2021-10-19 14:07:01 -0700

    Time: 112 ms (51.90%), Space: 44.7 MB (27.50%) - LeetHub

--- max-area-of-island/max-area-of-island.js
+++ max-area-of-island/max-area-of-island.js
@@ -3,40 +3,43 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    if (grid.length < 0) return 0;
+    // create a map to keep track of all the already seen square
+    // update the max land var after each patch is fully traversed
+    // use depth first search to see the traverse the path of land
     
-    const visited = new Set(); 
+    // at end of traversal of each square return max
+    
+    let visited = new Set(); 
     let maxArea = 0; 
     
-    for (let r = 0; r < grid.length; r++) {
-        for (let c = 0; c < grid[0].length; c++) {
+    for(let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[r].length; c++) {
             if (grid[r][c] === 1) {
-                maxArea = Math.max(maxArea, explore(grid, r, c, visited));   
+                let landArea = explore(grid, r, c, visited, 0); 
+                maxArea = Math.max(maxArea, landArea);
             }
         }
     }
-    
     return maxArea;
+
 };
 
-const explore = (graph, r, c, visited) => {
-    // base cases
-    // in bounds
-    if (r < 0 || c < 0 || r >= graph.length || c >= graph[0].length || graph[r][c] === 0) {
-        return 0;
-    }
+const explore = (grid, r, c, visited, area) => {
+    // base case
+    // out of bounds 
+    if (r < 0 || c < 0 || r >= grid.length || c >= grid[r].length || grid[r][c] === 0) return 0; 
     
-    // already seen
-    const position = r + ',' + c;
-    if (visited.has(position)) return 0;
-    visited.add(position);
+    // already visited
+    let node = r + ',' + c;
+    if (visited.has(node)) return 0;
+    visited.add(node);
     
-    let count = 1; 
+    area = 1;
     
-    count += explore(graph, r + 1, c, visited);
-    count += explore(graph, r - 1, c, visited);
-    count += explore(graph, r, c + 1, visited);
-    count += explore(graph, r, c - 1, visited);
+    area += explore(grid, r + 1, c, visited, area);
+    area += explore(grid, r - 1, c, visited, area);
+    area += explore(grid, r, c + 1, visited, area);
+    area += explore(grid, r, c - 1, visited, area);
     
-    return count; 
+    return area;
 };
