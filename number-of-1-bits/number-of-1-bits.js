58e99ca404152a9f33c810b72a683f08c8dfd962
Javier Zarate <javalzar17@gmail.com>
2021-10-24 13:18:52 -0700

    Time: 107 ms (36.06%), Space: 40.1 MB (62.95%) - LeetHub

--- number-of-1-bits/number-of-1-bits.js
+++ number-of-1-bits/number-of-1-bits.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    let sum = 0; 
+    let bits = 0; 
     
     while (n !== 0) {
-        sum++;
-        n &= (n - 1);
+        if (n & 1) bits++; 
+        n = n >>> 1;
     }
-    return sum;
+    return bits;
 };
-
-//1011
-//1010 -> 1010
-//        1001 -> 1001
-//                1000 -> 1000
-//                        0111 -> 0000
-//                             
