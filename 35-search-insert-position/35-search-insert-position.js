ff8fae0e5c0f2b925ae125a74fb3bbdbe34945c4
Javier Zarate <javalzar17@gmail.com>
2022-01-19 19:17:07 -0800

    Time: 141 ms (5.04%), Space: 39.3 MB (85.39%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -8,14 +8,16 @@ var searchInsert = function(nums, target) {
     let start = 0, end = nums.length - 1;
     
     while (start <= end) {
-        let mid = Math.floor(start + (end - start)/ 2);
+        let mid = Math.floor(start + (end - start) / 2);
         
-        if (nums[mid] === target) return mid; 
+        if (nums[mid] === target) return mid;
+        
+        if (nums[mid] < target) {
+            start = mid + 1;
+        }
         
         if (nums[mid] > target) {
             end = mid - 1;
-        } else {
-            start = mid + 1;
         }
     }
     return start;
